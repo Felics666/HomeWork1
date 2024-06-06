28a72a3d2f1bd18180c45d58ba0286e3a762a896
felics <felics6@gmail.com>
2024-06-07 00:24:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//////Задача 1:
+    System.out.println("Задача № 1");
+        var dog = 8.0;
+         System.out.println("dog - " + dog);
+
+           var cat = 3.6;
+            System.out.println("cat - " + cat);
+
+              var paper = 763789;
+               System.out.println ("paper - " + paper);
+/////Задача 2:
+    System.out.println("Задача № 2");
+         dog = dog + 4;
+          System.out.println ("dog - " + dog);
+
+          cat = cat + 4;
+           System.out.println ("cat - " + cat);
+
+            paper = paper + 4;
+             System.out.println ("paper - " + paper);
+
+/////Задача 3
+    System.out.println("Задача № 3");
+        dog = dog - 3.5;
+         System.out.println ("dog - " + dog);
+
+          cat = cat - 1.6;
+           System.out.println ("cat - " + cat);
+
+            paper = paper - 7639;
+             System.out.println ("paper - " + paper);
+
+/////Задача 4
+    System.out.println("Задача № 4");
+
+        var friend = 19;
+         System.out.println("friend - " + friend);
+
+          var rez = friend + 2;
+           System.out.println("friend = " + rez);
+
+            var rez2 = rez / 7;
+             System.out.println("friend = " + rez2);
+
+        }
+    }
